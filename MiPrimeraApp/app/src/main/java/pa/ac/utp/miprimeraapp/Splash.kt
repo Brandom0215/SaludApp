4d6e05ac09052b7d644beb0c127a79c3adc4d78e
Brandom0215 <brandom.arcia@utp.ac.pa>
2026-04-19 12:30:03 -0500

    Card View agregados

---
+++ MiPrimeraApp/app/src/main/java/pa/ac/utp/miprimeraapp/Splash.kt
@@ -0,0 +1,28 @@
+package pa.ac.utp.miprimeraapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@Splash, MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
