b91b9d71abb42e75089d4ad89b16ee372eb0028a
Brandom0215 <brandom.arcia@utp.ac.pa>
2026-04-20 08:21:37 -0500

    Pantallas por cada opcion en el main conectadas

--- MiPrimeraApp/app/src/main/java/pa/ac/utp/miprimeraapp/MainActivity.kt
+++ MiPrimeraApp/app/src/main/java/pa/ac/utp/miprimeraapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package pa.ac.utp.miprimeraapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.card.MaterialCardView
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,48 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val cardPeso = findViewById<MaterialCardView>(R.id.cardPeso)
+        val cardPresion = findViewById<MaterialCardView>(R.id.cardPresion)
+        val cardGlucosa = findViewById<MaterialCardView>(R.id.cardGlucosa)
+        val cardActividad = findViewById<MaterialCardView>(R.id.cardActividad)
+        val cardHidratacion = findViewById<MaterialCardView>(R.id.cardHidratacion)
+        val cardMedicacion = findViewById<MaterialCardView>(R.id.cardMedicacion)
+
+        cardPeso.setOnClickListener {
+            makeText(this, "Accediendo a Peso IMC", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Peso::class.java)
+            startActivity(intent)
+        }
+
+        cardPresion.setOnClickListener {
+            makeText( this,"Accediendo a Presion Arterial",  Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Presion::class.java)
+            startActivity(intent)
+        }
+
+        cardGlucosa.setOnClickListener {
+            makeText( this, "Accediendo a Control de Glucosa", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Glucosa::class.java)
+            startActivity(intent)
+        }
+
+        cardActividad.setOnClickListener {
+            makeText(this,"Accediendo a Control de Actividad Fisica",  Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Actividad::class.java)
+            startActivity(intent)
+        }
+
+        cardHidratacion.setOnClickListener {
+            makeText( this, "Accediendo a Control de Hidratacion",  Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Hidratacion::class.java)
+            startActivity(intent)
+        }
+
+        cardMedicacion.setOnClickListener {
+            makeText(this, "Accediendo a Medicamentos",  Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Medicacion::class.java)
+            startActivity(intent)
         }
     }
 }
